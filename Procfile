e6dfd53bfb070cfcb804ae861f057af58546b9af
Stephon94 <micronixon30@gmail.com>
2017-08-28 14:28:08 -0400

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn afrojuvenation.wsgi --log-file - 
---
+++ afrojuvenation/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'tut_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('portfolio.urls')),
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
